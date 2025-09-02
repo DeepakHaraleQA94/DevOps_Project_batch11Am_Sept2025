49712c0c38f1d8fd47dbd08f3c2e7b556860717b
DeepakH <haraledeepak6@gmail.com>
2025-09-02 10:36:29 +0530

    new repo

---
+++ src/main/java/DevopsProject_Automation/ValidateLoginPage.java
@@ -0,0 +1,14 @@
+package DevopsProject_Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ValidateLoginPage {
+
+	@Test
+	public void test() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.yessinfotech.com/");
+	}
+}
---
+++ src/main/java/DevopsProject_Automation/validateHomePage.java
@@ -0,0 +1,14 @@
+package DevopsProject_Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class validateHomePage {
+	@Test
+	public void test1() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.yessinfotech.com/");
+	}
+}
