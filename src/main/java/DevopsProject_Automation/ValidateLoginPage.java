3ebf937339cacbd11973a7b3c001cca017ef9e18
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-09-10 10:58:00 +0530

    new

--- src/main/java/DevopsProject_Automation/ValidateLoginPage.java
+++ src/main/java/DevopsProject_Automation/ValidateLoginPage.java
@@ -2,6 +2,7 @@ package DevopsProject_Automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ValidateLoginPage {
@@ -10,6 +11,15 @@ public class ValidateLoginPage {
 	public void test() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.yessinfotech.com/");
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+	
+		
+		Assert.assertEquals(title, "Software training classes institute in Hadapsar Pune123");
+		System.out.println("successfully landing home page");
+		
 		driver.close();
 	}
 }
